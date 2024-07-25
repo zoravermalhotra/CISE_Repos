b948b72e550341e2fd352884ffc32e298f6ea91e
zoravermalhotra <163698706+zoravermalhotra@users.noreply.github.com>
2024-07-25 20:46:22 +1200

    second commit

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+const div = document.createElement('div');
+ReactDOM.render(<App />, div);
+ReactDOM.unmountComponentAtNode(div);
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+  expect(2 + 2).toBe(4);
+  });
+  });
+  
 });
