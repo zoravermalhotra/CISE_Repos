34efbd37176ef004543ba044c086a0cbf2c36b5c
zoravermalhotra <163698706+zoravermalhotra@users.noreply.github.com>
2024-07-25 20:52:31 +1200

    third commit

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -5,10 +5,13 @@ it('renders without crashing', () => {
 const div = document.createElement('div');
 ReactDOM.render(<App />, div);
 ReactDOM.unmountComponentAtNode(div);
+
+
+  
+});
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
   expect(2 + 2).toBe(4);
   });
   });
   
-});
